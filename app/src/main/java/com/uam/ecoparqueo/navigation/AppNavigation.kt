244111320e794c652896a3efb701e1bc2a0fd1be
FranciscoGuevara9128 <fjguevara@uamv.edu.ni>
2026-04-29 23:13:51 -0600

    feat: Implementación de flujo de seguridad (Selección y Registro de Placas)

--- app/src/main/java/com/uam/ecoparqueo/navigation/AppNavigation.kt
+++ app/src/main/java/com/uam/ecoparqueo/navigation/AppNavigation.kt
@@ -1,2 +1,11 @@
 package com.uam.ecoparqueo.navigation
 
+import kotlinx.serialization.Serializable
+
+@kotlinx.serialization.Serializable
+object SecuritySelectionScreen // Pantalla 1 del Guarda
+
+@Serializable
+data class ControlAccesoVehicularScreen(
+    val nombreParqueo: String // Pasa el nombre del parqueo seleccionado
+)
