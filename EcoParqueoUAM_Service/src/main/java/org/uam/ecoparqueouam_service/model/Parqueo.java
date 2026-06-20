61bdddd164500f7800aa6ce35260a53efc5295df
FranciscoGuevara9128 <fjguevara@uamv.edu.ni>
2026-06-20 13:57:38 -0600

    fix: agregar Parqueo.java

--- EcoParqueoUAM_Service/src/main/java/org/uam/ecoparqueouam_service/model/Parqueo.java
+++ EcoParqueoUAM_Service/src/main/java/org/uam/ecoparqueouam_service/model/Parqueo.java
@@ -1,5 +1,47 @@
 package org.uam.ecoparqueouam_service.model;
 
-public class Parqueo {
-    
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="parqueo")
+
+public class Parqueo extends BaseEntity{
+    private String name;
+    private Integer  capacidadTotal;
+    private Integer disponibles;
+    private String direccion;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCapacidadTotal() {
+        return capacidadTotal;
+    }
+
+    public void setCapacidadTotal(Integer capacidadTotal) {
+        this.capacidadTotal = capacidadTotal;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getDisponibles() {
+        return disponibles;
+    }
+
+    public void setDisponibles(Integer disponibles) {
+        this.disponibles = disponibles;
+    }
 }
