1c81b71ab686e762139fefe0d9e7000888ee3b70
FranciscoGuevara9128 <fjguevara@uamv.edu.ni>
2026-06-20 18:17:59 -0600

    fix: cambiar id a nullable en parqueo y vehiculo para que gson lo omita en post y hibernate genere el uuid

--- EcoParqueo-UAM/app/src/main/java/com/uam/ecoparqueo/model/Parqueo.kt
+++ EcoParqueo-UAM/app/src/main/java/com/uam/ecoparqueo/model/Parqueo.kt
@@ -3,7 +3,7 @@ package com.uam.ecoparqueo.model
 import com.google.gson.annotations.SerializedName
 
 data class Parqueo(
-    @SerializedName("id") val id: String = "",
+    @SerializedName("id") val id: String? = null,
     @SerializedName("name") val name: String,
     @SerializedName("capacidadTotal") val capacidadTotal: Int,
     @SerializedName("disponibles") val disponibles: Int,
