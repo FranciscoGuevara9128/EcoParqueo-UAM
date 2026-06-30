cb7232a4cf6817c552accabccea299ec4c6281e5
FranciscoGuevara9128 <fjguevara@uamv.edu.ni>
2026-06-29 22:58:06 -0600

    feat(style): integrar Google Fonts y configurar Plus Jakarta Sans globalmente

--- EcoParqueo-UAM/app/src/main/java/com/uam/ecoparqueo/ui/theme/Type.kt
+++ EcoParqueo-UAM/app/src/main/java/com/uam/ecoparqueo/ui/theme/Type.kt
@@ -4,31 +4,43 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.uam.ecoparqueo.R
 
-// Set of Material typography styles to start with
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Plus Jakarta Sans")
+
+val PlusJakartaSans = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Light),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Normal),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Medium),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.SemiBold),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Bold),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.ExtraBold)
+)
+
+val defaultTypography = Typography()
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = PlusJakartaSans),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = PlusJakartaSans),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = PlusJakartaSans),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = PlusJakartaSans),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = PlusJakartaSans),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = PlusJakartaSans),
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = PlusJakartaSans),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = PlusJakartaSans),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = PlusJakartaSans),
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = PlusJakartaSans),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = PlusJakartaSans),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = PlusJakartaSans),
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = PlusJakartaSans),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = PlusJakartaSans),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = PlusJakartaSans)
 )
